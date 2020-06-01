2caaf797967377f3002308a66be33f902cc522c5
Marcin Michnik <marcin.michnik@o2.pl>
2020-06-01 13:24:34 +0200

    counter

--- n-grammer.js
+++ n-grammer.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(".input");
 input.innerText = "Enter text here";
+let numberOutput = 0;
 
 function nGram() {
     let e = document.getElementById("idSelect");
@@ -14,6 +15,7 @@ function nGram() {
         var splitTable = [];
         var nGrams = []
         var index
+
 
         if (value === null || value === undefined) {
             return nGrams
@@ -31,25 +33,30 @@ function nGram() {
             while (index--) {
                 nGrams[i] = '\n' + splitTable[i] + ' ' + splitTable[i + 1];
                 i++;
+                numberOutput++;
             }
         } else if (n === '3') {
             while (index--) {
                 nGrams[i] = '\n' + splitTable[i] + ' ' + splitTable[i + 1] + ' ' + splitTable[i + 2];
                 i++;
+                numberOutput++;
             }
         } else if (n === '4') {
             while (index--) {
                 nGrams[i] = '\n' + splitTable[i] + ' ' + splitTable[i + 1] + ' ' + splitTable[i + 2] + ' ' + splitTable[i + 3];
                 i++;
+                numberOutput++;
             }
         } else if (n === '5') {
             while (index--) {
                 nGrams[i] = '\n' + splitTable[i] + ' ' + splitTable[i + 1] + ' ' + splitTable[i + 2] + ' ' + splitTable[i + 3] + ' ' + splitTable[i + 4];
                 i++;
+                numberOutput++;
             }
         }
 
         let output = document.getElementById('output');
-        output.value = nGrams;
+        output.innerHTML = 'Count: '+String(numberOutput) + "\n" + String(nGrams);
+        numberOutput=0;
     }
 }
